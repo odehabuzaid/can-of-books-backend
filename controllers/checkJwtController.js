c6a45d2246f88c360e67fdde4d3184ef13da92e9
Odeh <abuzaid163@hotmail.com>
2021-08-17 09:04:50 +0300

    heroku app sudden crash - fix try 1

--- controllers/checkJwtController.js
+++ controllers/checkJwtController.js
@@ -8,26 +8,26 @@ const configs = getConfig();
 const {addTheBooks} = require('./dataControllers')
 
 function checkJwtController(request, response) {
-  let auth = request.headers.authorization.split(' ')[1];
-  JsonWebToken.verify(auth, getKey, {}, (error, user) => {
-    if (error) {
-      response.send(error);
-    } else {
-      response.send(user);
-      addTheBooks(user.email);
-    }
-  });
+    let auth = request.headers.authorization.split(' ')[1];
+    JsonWebToken.verify(auth, getKey, {}, (error, user) => {
+        if (error) {
+            response.send(error);
+        } else {
+            response.send(user);
+            addTheBooks(user.email);
+        }
+    });
 }
 
 function getKey (header, callback){
-      const client = jwksClient({
+    const client = jwksClient({
         jwksUri: `https://${configs.authinticationDomain}/.well-known/jwks.json`,
-      });
-      client.getSigningKey(header.kid, function (err, key) {
+    });
+    client.getSigningKey(header.kid, function (err, key) {
         const signInKey = key.publicKey || key.rsaPublicKey;
         callback(null, signInKey);
-      });
-    }
+    });
+}
 
 
 module.exports = {checkJwtController,configs};
